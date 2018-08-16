5efc7ef1df20e83faed4a019afbee344ea2c7488
hyunyoung2 <hyun02.engineer@gmail.com>
2018-08-16 22:13:16 +0900

    Download shell script for model trained

---
+++ downloader.sh
@@ -0,0 +1,17 @@
+#/usr/bin/env bash 
+
+# this shell script is downloader for model trained. 
+
+echo "downloading........"
+echo
+
+MODEL="model.tar.gz"
+
+curl -O "http://nlp.kookmin.ac.kr/hyunyoung2/compound_noun/${MODEL}"
+
+echo "uncompressing......"
+echo 
+
+tar -xvzf ${MODEL}
+
+rm ${MODEL}
